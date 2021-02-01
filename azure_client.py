cff6b0cf124def68cb54dbe6d414170d87639be7
JMFrmg <matthieu.colombert@gmail.com>
2021-02-01 14:26:06 +0100

    gestions des clés v2

--- azure_client.py
+++ azure_client.py
@@ -5,23 +5,24 @@ from azure.ai.textanalytics import TextAnalyticsClient
 from azure.core.credentials import AzureKeyCredential
 from msrest.authentication import CognitiveServicesCredentials
 
-
-def form_recognizer_client():
+def get_api_creds(ressource):
     f = open("keys.txt", "r")
     keys = f.readlines()
     f.close()
-    form_recognizer_endpoint = "https://matthieuc-form-recognizer.cognitiveservices.azure.com/"
-    form_recognizer_key = keys[0].strip()
-    client = FormRecognizerClient(form_recognizer_endpoint,
+    creds_dict = {l.split(" ")[0].strip(): l.split(" ")[1].strip() for l in keys}
+    return (creds_dict[ressource + "_endpoint"], creds_dict[ressource + "_key"])
+
+def form_recognizer_client():
+    creds = get_api_creds("form_recognizer")
+    form_recognizer_key = creds[1]
+    client = FormRecognizerClient(creds[0],
                                   AzureKeyCredential(form_recognizer_key))
     return client
 
 def computer_vision_client():
-    f = open("keys.txt", "r")
-    keys = f.readlines()
-    f.close()
-    computer_vision_key = keys[1].strip()
-    computer_vision_endpoint = "https://matthieuc-recipe-computer-vision.cognitiveservices.azure.com/"
+    creds = get_api_creds("computer_vision")
+    computer_vision_key = creds[1]
+    computer_vision_endpoint = creds[0]
     client = ComputerVisionClient(computer_vision_endpoint,
                                   CognitiveServicesCredentials(computer_vision_key))
     return client
