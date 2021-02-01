103cf4e49e90ecfc2e735568546492835269a89f
JMFrmg <matthieu.colombert@gmail.com>
2021-02-01 14:05:54 +0100

    keys un file

--- azure_client.py
+++ azure_client.py
@@ -7,14 +7,20 @@ from msrest.authentication import CognitiveServicesCredentials
 
 
 def form_recognizer_client():
+    f = open("keys.txt", "r")
+    keys = f.readlines()
+    f.close()
     form_recognizer_endpoint = "https://matthieuc-form-recognizer.cognitiveservices.azure.com/"
-    form_recognizer_key = "107538cdca8b45d3851402c9363519c1"
+    form_recognizer_key = keys[0].strip()
     client = FormRecognizerClient(form_recognizer_endpoint,
                                   AzureKeyCredential(form_recognizer_key))
     return client
 
 def computer_vision_client():
-    computer_vision_key = "621e3f4f1e1041b3824cb77f4390e2bd"
+    f = open("keys.txt", "r")
+    keys = f.readlines()
+    f.close()
+    computer_vision_key = keys[1].strip()
     computer_vision_endpoint = "https://matthieuc-recipe-computer-vision.cognitiveservices.azure.com/"
     client = ComputerVisionClient(computer_vision_endpoint,
                                   CognitiveServicesCredentials(computer_vision_key))
